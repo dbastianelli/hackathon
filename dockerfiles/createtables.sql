e1e9e6a5049f5de5d92d02437ea292eefc7da4d9
murugesanponnusamy <magnus.cowie@googlemail.comcat>
2025-04-01 11:49:53 +0000

    dockerfiles

---
+++ dockerfiles/createtables.sql
@@ -0,0 +1,4 @@
+create table emp (column1 varchar(20), column2 varchar(20));
+create table emp1 (column1 varchar(20), column2 varchar(20));
+create table emp2 (column1 varchar(20), column2 varchar(20));
+create table emp3 (column1 varchar(20), column2 varchar(20));
---
+++ dockerfiles/insertvalues.sql
@@ -0,0 +1,4 @@
+insert into emp values('A1','B1');
+insert into emp values('A2','B2');
+insert into emp values('A3','B3');
+insert into emp values('A4','B4');
